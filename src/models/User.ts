2c2b78f958b57b9a05adb96136887d0a1b0535e0
Manuel_Perez_Reyes <manuel@disacomp.com>
2025-03-21 20:44:04 -0400

    entendiendo el modelo de la base de datos

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,35 +1,7 @@
-import mongoose, { mongo, Schema } from "mongoose";
-
-interface IUser {
-  handle: string;
-  email: string;
-  name: string;
-  password: string;
-}
+import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema({
-  handle: {
-    type: String,
-    require: true,
-    trim: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    require: true,
-    trim: true,
-    unique: true,
-  },
-  name: {
-    type: String,
-    require: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    require: true,
-    trim: true,
-  },
-});
-
-export const User = mongoose.model<IUser>("User", userSchema);
+  "name":{
+    
+  }
+})
