145b420e1a8dae6d497a7913afc3f9de947c97a4
Manuel_Perez_Reyes <manuel.p.r@hotmail.com>
2025-03-28 10:35:10 -0400

    Refactor createAccount handler to improve code readability and remove unnecessary whitespace

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,14 +1,14 @@
 import { Response, Request } from "express";
-import slug from 'slug'; 
+import slug from 'slug';
 
 import { User } from '../models/User';
 import { hashPassword } from "../utils/auth";
 
 const createAccount = async (req: Request, res: Response) => {
     try {
-    
+
         const { email, handle, password } = req.body;
-    
+
         const userExist = await User.findOne({ email });
         if (!!userExist) {
             res.status(409).json({
@@ -16,9 +16,9 @@ const createAccount = async (req: Request, res: Response) => {
             })
             return
         }
-    
+
         //validar que el handle no exista (instalar slug)
-        const handleSlug = slug(handle, '_');
+        const handleSlug = slug(handle, '');
         const existHandle = await User.findOne({ handle: handleSlug });
         if (!!existHandle) {
             res.status(409).json({
@@ -26,22 +26,26 @@ const createAccount = async (req: Request, res: Response) => {
             })
             return
         }
-    
+
         //Crear una nueva instancia de User
         let user = new User(req.body);
-            
+
         user.password = await hashPassword(password);
-    
+
         user.handle = handleSlug;
-    
+
         user.save();
-    
+
         res.send('Usuario registrado correctamente');
-        
+
     } catch (error) {
         console.log(error.message)
     }
 }
+
+// const getAccountById = async(req:Request, res:Response) => {
+//     const users = User.findById(req.user.id)
+// }
 
 export {
     createAccount
